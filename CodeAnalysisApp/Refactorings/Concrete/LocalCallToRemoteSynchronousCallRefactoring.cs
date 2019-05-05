3ebc59c1bfb784bf20e0e81528a49aead2de596f
João Pinto <joao.paiva@egplusww.com>
2019-05-05 17:32:47 +0100

    Refactored code structure.

---
+++ CodeAnalysisApp/Refactorings/Concrete/LocalCallToRemoteSynchronousCallRefactoring.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.CodeAnalysis;
+
+namespace CodeAnalysisApp.Refactorings.Concrete
+{
+	public class LocalCallToRemoteSynchronousCallRefactoring : IRefactoringStrategy
+	{
+		public LocalCallToRemoteSynchronousCallRefactoring() { }
+
+		public void ApplyRefactoring(Solution workspace)
+		{			
+			// Debug
+
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ CodeAnalysisApp/Refactorings/IRefactoringStrategy.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace CodeAnalysisApp.Refactorings
+{
+	public interface IRefactoringStrategy
+	{
+		void ApplyRefactoring(Solution workspace);
+	}
+}
