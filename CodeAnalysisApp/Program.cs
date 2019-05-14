3db3b29dc3e5e930d37ceb5e08472013506b7934
João Pinto <joao.paiva@egplusww.com>
2019-05-15 00:29:55 +0100

    Progress.

--- CodeAnalysisApp/Program.cs
+++ CodeAnalysisApp/Program.cs
@@ -1,3 +1,4 @@
+using CodeAnalysisApp.Analyzer;
 using CodeAnalysisApp.Writer;
 using System.Threading.Tasks;
 
@@ -7,10 +8,12 @@ namespace CodeAnalysisApp
 	{
 		static async Task Main(string[] args)
 		{
-			// await new RefactoringsAnalyzer().BeginAnalysis();
+			await new RefactoringsAnalyzer().BeginAnalysis();
 			new RefactoringsWriter().BeginWriting();
 		}
 	}
 
 	// TODO: Usar HTTP POST sempre a rota fica o nome da classe mais o nome do método.
+	// TODO: Ir buscar todas as classes da solution e depois quando se está a ir buscar cada um dos objetos 
+	// usados na classe verificar se fazem parte dessa primeira lista. Hope this makes sense tomorrow :)
 }
--- CodeAnalysisApp/Refactorings/Concrete/LocalCallToRemoteSynchronousCallRefactoring.cs
+++ CodeAnalysisApp/Refactorings/Concrete/LocalCallToRemoteSynchronousCallRefactoring.cs
@@ -1,7 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Classification;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace CodeAnalysisApp.Refactorings.Concrete
 {
@@ -14,27 +18,76 @@ namespace CodeAnalysisApp.Refactorings.Concrete
 			// Debug
 			var project = solution.Projects.Where(p => p.Name == "MonolithDemo").FirstOrDefault();
 
+
+
+			//var compilation = await project.GetCompilationAsync();
+
+			//var classVisitor = new ClassVirtualizationVisitor();
+			//classVisitor.Visit(semanticModel.SyntaxTree.getRoot());
+
+			//var classes = classVisitor.classes;
+
 			var document = project.Documents.Where(d => d.Name == "RoomsService.cs").FirstOrDefault();
+
+			var semanticModel = document.GetSemanticModelAsync();
+
+			//var classVisitor = new ClassVirtualizationVisitor();
+			//classVisitor.Visit(semanticModel.);
 
 			if (document.SupportsSyntaxTree)
 			{
 				var syntaxTree = await document.GetSyntaxTreeAsync();
 				var treeRoot = await syntaxTree.GetRootAsync();
 
+				var list1 = treeRoot.DescendantNodes().OfType<SimpleNameSyntax>().ToList();
+				var list2 = treeRoot.DescendantNodes().OfType<MemberDeclarationSyntax>().ToList();
+				var list3 = treeRoot.DescendantNodes().OfType<CompilationUnitSyntax>().ToList();
+				var classDeclaration = treeRoot.DescendantNodes().OfType<ClassDeclarationSyntax>().ToList().FirstOrDefault();
+
+				var list5 = treeRoot.DescendantNodes().OfType<ClassOrStructConstraintSyntax>().ToList();
+				var list6 = new HashSet<IdentifierNameSyntax>(classDeclaration.DescendantNodes().OfType<IdentifierNameSyntax>().ToList());
+				var list7 = treeRoot.DescendantNodes().OfType<TypeSyntax>().ToList();
+
+				var classVisitor = new ClassVirtualizationVisitor();
+				//classVisitor.Visit(treeRoot);
+
+				//var classes = classVisitor.classes; // li
+
+				//foreach (UsingDirectiveSyntax element in root.Usings)
+				//{
+				//	var bc = element;
+				//}
+				//Classifier.GetClassifiedSpans()
+
 				var a = treeRoot.ChildNodes();
 
 				var b = treeRoot.DescendantNodes();
 
-				Console.WriteLine(syntaxTree);
+				//Console.WriteLine(syntaxTree);
 			}
 
-			if (document.SupportsSemanticModel)
-			{
-				var semanticModel = await document.GetSemanticModelAsync();
-				Console.WriteLine(semanticModel);
-			}
+			//if (document.SupportsSemanticModel)
+			//{
+			//	var semanticModel = await document.GetSemanticModelAsync();
+			//	Console.WriteLine(semanticModel);
+			//}
 
-			throw new NotImplementedException();
+			//throw new NotImplementedException();
+		}
+	}
+
+	public class ClassVirtualizationVisitor : CSharpSyntaxRewriter
+	{
+		public List<string> classes = new List<string>();
+
+		public override SyntaxNode VisitClassDeclaration(ClassDeclarationSyntax node)
+		{
+			node = (ClassDeclarationSyntax)base.VisitClassDeclaration(node);
+
+			string className = node.Identifier.ValueText;
+			classes.Add(className); // save your visited classes
+
+			return node;
 		}
 	}
 }
