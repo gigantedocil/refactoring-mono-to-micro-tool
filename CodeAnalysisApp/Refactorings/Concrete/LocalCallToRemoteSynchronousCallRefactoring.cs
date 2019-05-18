34054c83e51b03416e71c8c0b1f41c11671afb6f
João Pinto <joao.paiva@egplusww.com>
2019-05-18 20:00:45 +0100

    WIP.

--- CodeAnalysisApp/Refactorings/Concrete/LocalCallToRemoteSynchronousCallRefactoring.cs
+++ CodeAnalysisApp/Refactorings/Concrete/LocalCallToRemoteSynchronousCallRefactoring.cs
@@ -11,9 +11,7 @@ namespace CodeAnalysisApp.Refactorings.Concrete
 {
 	public class LocalCallToRemoteSynchronousCallRefactoring : IRefactoringStrategy
 	{
-		private ISet<Document> SolutionDocuments = new HashSet<Document>();
-
-		private ISet<DocumentAnalyzerAggregate> documentsRegistry = new HashSet<DocumentAnalyzerAggregate>();
+		private ISet<DocumentAnalyzerAggregate> documentsRegistry;
 
 		private readonly string ProjectName = "MonolithDemo";
 
@@ -23,86 +21,22 @@ namespace CodeAnalysisApp.Refactorings.Concrete
 
 		public async Task ApplyRefactoring(Solution solution)
 		{
-			await InitializeDocumentRegistry(solution);
+			documentsRegistry = await InitializeDocumentRegistry(solution);
 
-			await GetInvokationMethodType(solution);
-		}
-
-		private void GetSolutionClasses(Solution solution)
-		{
-			foreach (var project in solution.Projects)
-			{
-				foreach (var document in project.Documents)
-				{
-					SolutionDocuments.Add(document);
-				}
-			}
-		}
-
-		private async Task GetInvokationMethodType(Solution solution)
-		{
-			var project = solution.Projects.Where(p => p.Name == ProjectName).FirstOrDefault();
-
-			var document = project.Documents.Where(d => d.Name == ClassName).FirstOrDefault();
-
-			var semanticModel = await document.GetSemanticModelAsync();
-
-			var syntaxTree = await document.GetSyntaxTreeAsync();
-
-			var methodInvocations = syntaxTree.GetRoot().DescendantNodes().OfType<InvocationExpressionSyntax>();
-
-			var invokedMethod = methodInvocations.FirstOrDefault(x => x.ToString().Contains(MethodName));
-
-			var invokedMethodMetadata = semanticModel.GetSymbolInfo(invokedMethod).Symbol;
-
-			var typeFullName = invokedMethodMetadata.ContainingType.ToDisplayString();
-
-			var invokedMethodDocument = documentsRegistry.FirstOrDefault(x => x.DocumentTypeFullName == typeFullName);
+			var invokedMethodDocument = await GetInvokationMethodType(solution);
 
 			await RecursiveMethod(invokedMethodDocument, solution);
 		}
 
-		private async Task RecursiveMethod(DocumentAnalyzerAggregate document, Solution solution)
+		private async Task<HashSet<DocumentAnalyzerAggregate>> InitializeDocumentRegistry(Solution solution)
 		{
-			var treeRoot = await document.SyntaxTree.GetRootAsync();
+			var documents = new HashSet<DocumentAnalyzerAggregate>();
 
-			// TODO: Get syntax tree of type so I can get all classes of that file and do that recursively.
-
-			// If is class declaration buscar todas as classes if interface buscar interface implementations e 
-			// We're assuming each file only has a class
-			var isClass = treeRoot.DescendantNodes().OfType<ClassDeclarationSyntax>().FirstOrDefault() != null;
-
-			var interfaceest = treeRoot.DescendantNodes().OfType<InterfaceDeclarationSyntax>().FirstOrDefault();
-
-			if (isClass)
-			{
-
-			}
-
-			if (interfaceest != null)
-			{
-
-				var interfaceSymbol = document.SemanticModel.GetSymbolInfo(interfaceest).Symbol;
-
-				var implementations = await SymbolFinder.FindImplementationsAsync(interfaceSymbol, solution);
-			}
-
-			var classDeclaration = treeRoot.DescendantNodes().OfType<ClassDeclarationSyntax>().ToList().FirstOrDefault();
-
-			var list6 = new HashSet<IdentifierNameSyntax>(classDeclaration.DescendantNodes().OfType<IdentifierNameSyntax>().ToList());
-			var list7 = treeRoot.DescendantNodes().OfType<TypeSyntax>().ToList();
-
-			// If it is an interface get implementations.
-			// Otherwise search for types inside types.
-		}
-
-		private async Task InitializeDocumentRegistry(Solution solution)
-		{
 			foreach (var project in solution.Projects)
 			{
 				foreach (var document in project.Documents)
 				{
-					documentsRegistry.Add(
+					documents.Add(
 						new DocumentAnalyzerAggregate()
 						{
 							Document = document,
@@ -114,6 +48,7 @@ namespace CodeAnalysisApp.Refactorings.Concrete
 				}
 			}
 
+			return documents;
 		}
 
 		private async Task<string> GetNameFromDocument(Document document)
@@ -155,6 +90,63 @@ namespace CodeAnalysisApp.Refactorings.Concrete
 			}
 
 			return nameSpace + "." + typeName;
+		}
+
+		private async Task<DocumentAnalyzerAggregate> GetInvokationMethodType(Solution solution)
+		{
+			var project = solution.Projects.Where(p => p.Name == ProjectName).FirstOrDefault();
+
+			var document = project.Documents.Where(d => d.Name == ClassName).FirstOrDefault();
+
+			var semanticModel = await document.GetSemanticModelAsync();
+
+			var syntaxTree = await document.GetSyntaxTreeAsync();
+
+			var methodInvocations = syntaxTree.GetRoot().DescendantNodes().OfType<InvocationExpressionSyntax>();
+
+			var invokedMethod = methodInvocations.FirstOrDefault(x => x.ToString().Contains(MethodName));
+
+			var invokedMethodMetadata = semanticModel.GetSymbolInfo(invokedMethod).Symbol;
+
+			var typeFullName = invokedMethodMetadata.ContainingType.ToDisplayString();
+
+			var invokedMethodDocument = documentsRegistry.FirstOrDefault(x => x.DocumentTypeFullName == typeFullName);
+
+			return invokedMethodDocument;
+		}
+
+		private async Task RecursiveMethod(DocumentAnalyzerAggregate document, Solution solution)
+		{
+			var treeRoot = await document.SyntaxTree.GetRootAsync();
+
+			// TODO: Get syntax tree of type so I can get all classes of that file and do that recursively.
+
+			// If is class declaration buscar todas as classes if interface buscar interface implementations e 
+			// We're assuming each file only has a class
+			var isClass = treeRoot.DescendantNodes().OfType<ClassDeclarationSyntax>().FirstOrDefault() != null;
+
+			var interfaceest = treeRoot.DescendantNodes().OfType<InterfaceDeclarationSyntax>().FirstOrDefault();
+
+			if (isClass)
+			{
+
+			}
+
+			if (interfaceest != null)
+			{
+
+				var interfaceSymbol = document.SemanticModel.GetSymbolInfo(interfaceest).Symbol;
+
+				var implementations = await SymbolFinder.FindImplementationsAsync(interfaceSymbol, solution);
+			}
+
+			var classDeclaration = treeRoot.DescendantNodes().OfType<ClassDeclarationSyntax>().ToList().FirstOrDefault();
+
+			var list6 = new HashSet<IdentifierNameSyntax>(classDeclaration.DescendantNodes().OfType<IdentifierNameSyntax>().ToList());
+			var list7 = treeRoot.DescendantNodes().OfType<TypeSyntax>().ToList();
+
+			// If it is an interface get implementations.
+			// Otherwise search for types inside types.
 		}
 	}
 }
