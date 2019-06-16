3f5e4f6e63f06d866b96e7d0e92b76226652673f
João Paiva <jo.paiva.cp@gmail.com>
2019-06-16 23:16:59 +0100

    Fixed issue.

---
+++ CodeAnalysisApp.MonolithDemo/Services/Rooms/RoomsService.cs
@@ -0,0 +1,33 @@
+using MonolithDemo.Repositories.Rooms;
+using MonolithDemo.Services.Pricing;
+
+namespace MonolithDemo.Services.Rooms
+{
+	public class RoomsService : IRoomsService
+	{
+		private readonly IPricingService pricingService;
+		private readonly IRoomsRepository roomsRepository;
+
+		public RoomsService(
+			IPricingService pricingService,
+			IRoomsRepository roomsRepository)
+		{
+			this.pricingService = pricingService;
+			this.roomsRepository = roomsRepository;
+		}
+
+		public float GetRoomPricing(int roomId)
+		{
+			var roomType = roomsRepository.GetRoomsType(roomId);
+
+			var roomPrice = pricingService.CalculatePrice(roomType, "");
+
+			return roomPrice;
+		}
+
+		public bool RoomExists(int roomId)
+		{
+			return roomsRepository.RoomExists(roomId);
+		}
+	}
+}
